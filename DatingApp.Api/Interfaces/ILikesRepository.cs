b99c02af2d077f3f5d32edca59824deed6321a87
Apl9x <crvv002@gmail.com>
2021-11-01 07:30:28 -0600

    ILikesRepository

---
+++ DatingApp.Api/Interfaces/ILikesRepository.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.Api.DTOs;
+using DatingApp.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Api.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<AppUser> getUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+    }
+}
