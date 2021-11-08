0ab50453c0af16955ff97a8aa05e6ae18c83b7d0
Apl9x <crvv002@gmail.com>
2021-11-08 08:00:27 -0600

    GetThreadMessage and DeleteMessage

--- DatingApp.Api/Controllers/MessagesController.cs
+++ DatingApp.Api/Controllers/MessagesController.cs
@@ -63,5 +63,33 @@ namespace DatingApp.Api.Controllers
 
             return messages;
         }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+            return Ok(await _messagesRepository.GetMessageThread(currentUsername, username));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var username = User.GetUsername();
+            var message = await _messagesRepository.GetMessage(id);
+            if (message.Sender.UserName != username && message.Recipient.UserName != username)
+                return Unauthorized();
+
+
+            if (message.Sender.UserName == username) message.SenderDeleted = true;
+
+            if (message.Recipient.UserName == username) message.RecipientDeleted = true;
+
+            if (message.SenderDeleted && message.RecipientDeleted)
+                _messagesRepository.DeleteMessage(message);
+
+            if (await _messagesRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to delete the message");
+        }
     }
 }
