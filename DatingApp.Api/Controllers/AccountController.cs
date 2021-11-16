bda85e91c852fcfddfb5bf392954e5b1bdc6b971
Apl9x <crvv002@gmail.com>
2021-11-16 07:59:54 -0600

    Roles

--- DatingApp.Api/Controllers/AccountController.cs
+++ DatingApp.Api/Controllers/AccountController.cs
@@ -42,10 +42,15 @@ namespace DatingApp.Api.Controllers
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
             if (!result.Succeeded) return BadRequest(result.Errors);
+
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+            if (!roleResult.Succeeded) return BadRequest("Failed to add user to role: " + result.Errors);
+
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -67,7 +72,7 @@ namespace DatingApp.Api.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
---
+++ DatingApp.Api/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DatingApp.Api.DTOs;
+using DatingApp.Api.Entities;
+using DatingApp.Api.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Api.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public async Task<ActionResult> GetUsersWithRoles()
+        {
+            return Ok("Only admins can access this");
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public async Task<ActionResult> GetPhotosForModeration()
+        {
+            return Ok("ADmins or moderators can see this");
+        }
+    }
+}
